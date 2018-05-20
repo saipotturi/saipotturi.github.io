877c41da625cca6d881579e57550d09fbd705d66
Sai Potturi <spottu527@Sais-MacBook-Pro.local>
2018-05-20 17:59:30 -0400

    huge changes

--- alerts_app/js/script.js
+++ alerts_app/js/script.js
@@ -1,27 +1,87 @@
+( function (global) {
+
+var clusterTileUrl = "snippets/cluster-tile.html";
+
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+var insertProperty = function (string, propName, propValue) {
+  var propToReplace = "{{" + propName + "}}";
+  string = string
+    .replace(new RegExp(propToReplace, "g"), propValue);
+  return string;
+};
+
 //EVENT HANDLING
 document.addEventListener("DOMContentLoaded",
 	function (event) {
-		$ajaxUtils.sendGetRequest("data/Thunderbolt.txt", 
+
+		$ajaxUtils.sendGetRequest("data/cluster_list", 
+			loadClusterTiles);
+
+	//	$ajaxUtils.sendGetRequest("data/Thunderbolt.txt", 
+	//		loadHealth);
+
+	}
+);
+
+function loadClusterTiles(cluster_list)
+{
+	var clusters = cluster_list.responseText.split(/\r\n|\r|\n/);
+	var cluster_count = clusters.length;
+	for(String cluster_name : clusters)
+	{
+		$ajaxUtils.sendGetRequest(clusterTileUrl, 
 			function(request)
+			{
+				var tileHtml = insertProperty(request, cluster_variable, cluster_name);
+
+				insertHtml(cluster_row, tileHtml);
+			}
+		);
+
+		$ajaxUtils.sendGetRequest(`"data/${cluster_name}"`, function(request)
 			{
 				var count = request.responseText.split(/\r\n|\r|\n/).length;
 				if(count > 1)
 				{
-					document.querySelector("#Thunderbolt").style.backgroundColor = "red";
+					document.querySelector(`"#${cluster_name}"`).style.backgroundColor = "red";
 				}
 				else if(count == 1)
 				{
-					document.querySelector("#Thunderbolt").style.backgroundColor = "yellow";
+					document.querySelector("#${cluster_name}").style.backgroundColor = "yellow";
 				}
 				else
 				{
-					document.querySelector("#Thunderbolt").style.backgroundColor = "green";
+					document.querySelector("#${cluster_name}").style.backgroundColor = "green";
 				}
-				document.querySelector("#Thunderbolt").querySelector("p").innerHTML = count;
+				document.querySelector("#${cluster_name}").querySelector("p").innerHTML = count;
+			});
+	}
+}
+
+/*
+function loadHealth(request)
+{
+	var count = request.responseText.split(/\r\n|\r|\n/).length;
+	if(count > 1)
+	{
+		document.querySelector("#Thunderbolt").style.backgroundColor = "red";
+	}
+	else if(count == 1)
+	{
+		document.querySelector("#Thunderbolt").style.backgroundColor = "yellow";
+	}
+	else
+	{
+		document.querySelector("#Thunderbolt").style.backgroundColor = "green";
+	}
+	document.querySelector("#Thunderbolt").querySelector("p").innerHTML = count;
 				//document.querySelector("#Thunderbolt")
 				//	.innerHTML = request.responseText;
-			}
-		);
+}
+*/
 
-	}
-);
+})(window);
