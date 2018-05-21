d0ee6989f7dc1849ab399bed87f14d91695c29d5
Sai Potturi <spottu527@Sais-MacBook-Pro.local>
2018-05-21 16:35:45 -0400

    test4

--- alerts_app/js/script.js
+++ alerts_app/js/script.js
@@ -5,10 +5,43 @@
 document.addEventListener("DOMContentLoaded",
 	function (event) 
 	{
-		$ajaxUtils.sendGetRequest("data/Thunderbolt.txt", loadStatus);
+		$ajaxUtils.sendGetRequest("data/cluster_list", loadClustersInfo);
+		//$ajaxUtils.sendGetRequest("data/Thunderbolt.txt", loadStatus);
 
 	}
 );
+
+
+function loadClustersInfo(request)
+{
+	console.log(request);
+	var clusters = ["Thunderbolt"];
+	var cluster_count = clusters.length;
+	
+	for( var int i=0; i<cluster_count; i++)
+	{
+		$ajaxUtils.sendGetRequest(`data/${clusters[i]}.txt`, function(request)
+			{
+				var count = request.responseText.split(/\r\n|\r|\n/).length;
+				if(count > 1)
+				{
+					document.querySelector(`"#${clusters[i]}"`).style.backgroundColor = "red";
+				}
+				else if(count == 1)
+				{
+					document.querySelector(`"#${clusters[i]}"`).style.backgroundColor = "yellow";
+				}
+				else
+				{
+					document.querySelector(`"#${clusters[i]}"`).style.backgroundColor = "green";
+				}
+				document.querySelector(`"#${clusters[i]}"`).querySelector("p").innerHTML = count;
+			}
+		);
+	}
+
+	
+}
 
 
 function loadStatus(request)
