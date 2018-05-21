5bd52cf8552138887393d5fcf1d7368b7ca6ec70
Sai Potturi <spottu527@Sais-MacBook-Pro.local>
2018-05-21 16:00:36 -0400

    test3

--- alerts_app/js/script.js
+++ alerts_app/js/script.js
@@ -3,30 +3,33 @@
 (function (global) {
 //EVENT HANDLING
 document.addEventListener("DOMContentLoaded",
-function (event) 
-{
-	$ajaxUtils.sendGetRequest("data/Thunderbolt.txt", 
-	function(request)
+	function (event) 
 	{
-		var count = request.responseText.split(/\r\n|\r|\n/).length;
-		if(count > 1)
-		{
-			document.querySelector("#Thunderbolt").style.backgroundColor = "red";
-		}
-		else if(count == 1)
-		{
-			document.querySelector("#Thunderbolt").style.backgroundColor = "yellow";
-		}
-		else
-		{
-			document.querySelector("#Thunderbolt").style.backgroundColor = "green";
-		}
-		document.querySelector("#Thunderbolt").querySelector("p").innerHTML = count;
-				//document.querySelector("#Thunderbolt")
-				//	.innerHTML = request.responseText;
-	});
+		$ajaxUtils.sendGetRequest("data/Thunderbolt.txt", loadStatus);
+
+	}
+);
+
+
+function loadStatus(request)
+{
+	var count = request.responseText.split(/\r\n|\r|\n/).length;
+	if(count > 1)
+	{
+		document.querySelector("#Thunderbolt").style.backgroundColor = "red";
+	}
+	else if(count == 1)
+	{
+		document.querySelector("#Thunderbolt").style.backgroundColor = "yellow";
+	}
+	else
+	{
+		document.querySelector("#Thunderbolt").style.backgroundColor = "green";
+	}
+	document.querySelector("#Thunderbolt").querySelector("p").innerHTML = count;
 
 }
-);
+
+
 })(window);
 
