9941dbc2b6c4bc8d19d192e4963cb15f1f365ec5
Sai Potturi <spottu527@Sais-MacBook-Pro.local>
2018-05-22 17:09:20 -0400

    son of a gun 21

--- alerts_app_copy/js/script.js
+++ alerts_app_copy/js/script.js
@@ -66,7 +66,7 @@ function loadClusterTiles(cluster_list)
 	
 				if(count > 1)
 				{
-					document.querySelector(`#${cluster_name}`).style.backgroundColor = "red";
+					document.querySelector(`#"${cluster_name}"`).style.backgroundColor = "red";
 				}
 				else if(count == 1)
 				{
