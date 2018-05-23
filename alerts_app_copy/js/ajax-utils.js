611caf38c3e083f53b34b966a69864cebf5c7844
Sai Potturi <spottu527@Sais-MacBook-Pro.local>
2018-05-23 14:50:52 -0400

    son of a gun 57

--- alerts_app_copy/js/ajax-utils.js
+++ alerts_app_copy/js/ajax-utils.js
@@ -30,7 +30,7 @@ ajaxUtils.sendGetRequest =
                        responseHandler,
                        isJsonResponse); 
       };
-    request.open("GET", requestUrl, false);
+    request.open("GET", requestUrl, true);
     request.send(null); // for POST only
   };
 
